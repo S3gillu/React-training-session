aae190a8da169f83cb22e1de8719dd1219bb8ce3
S3gillu <shubhamsaurabh440@yahoo.com>
2020-08-25 19:15:51 +0530

    Routing module day 5

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import './App.css';
 
-import ComponentOne from './Components/ComponentOne';
-import CustomComponnet from './Components/CustomeComponnet';
+import Home from './Views/Home';
+import About from './Views/About';
+import Services from './Views/Services';
 
-const data = [
-  { id: 1, name: 'shubham', age: 20, openModal: true},
-  // { id: 2, name: 'amit', age: 25},
-  // { id: 3, name: 'snehal', age: 30},
-  // { id: 4, name: 'john', age: 20},
-  // { id: 5, name: 'mona', age: 15}
-]
 
 class App extends Component {
+
   render() {
     return (
       <>
-      {
-        data.map((data, index) => {
-          return (
-            <div>
-            <div key={data.id}>
-            <ComponentOne name={data.name} age={data.age} />
-            </div>
-            <CustomComponnet index={index}/>
-            </div>
-          )
-        })
-      }
+         <Router>
+          <Switch>
+            <Route exact={true} path="/home" component={Home} />
+            <Route exact={true} path="/about" component={About} />
+            <Route exact={true} path="/services" component={Services} />
+            <Redirect to="/home" />
+          </Switch>
+        </Router>
       </>
     )
   }
